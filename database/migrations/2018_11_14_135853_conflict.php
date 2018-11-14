30de8ed97534d07fe5a28fd0fd15f4f1cd2eda8c
SonNguyenTrong <trrongson1999@gmail.com>
2018-11-14 20:59:25 +0700

    first

---
+++ database/migrations/2018_11_14_135853_conflict.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Conflict extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table->increments('id');
+        $table->string('name');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
